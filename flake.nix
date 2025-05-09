3b85a02eea9e6af01bf0fbbaa4644c1fba10df7f
mow44 <mow44@proton.me>
2025-05-09 18:04:29 +0300

    Initial

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "slstatus-1.0";
+
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      defaultPackage.x86_64-linux =
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "slstatus";
+          version = "1.0"; # TODO 1.1
+
+          src = self;
+
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [
+            xorg.libX11
+            xorg.libXau
+            xorg.libXdmcp
+          ];
+
+          installFlags = [ "PREFIX=$(out)" ];
+
+          meta = {
+            mainProgram = "slstatus";
+          };
+        };
+    };
+}
